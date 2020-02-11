d38fc949075016c6d0b78a404bcc60478a5ac246
Ulric Qin <ulric.qin@gmail.com>
2020-02-11 12:11:44 +0800

    code refactor

--- src/dataobj/notify.go
+++
@@ -1,8 +0,0 @@
-package dataobj
-
-type NotifyMessage struct {
-	Tos     []string `json:"tos"`
-	Subject string   `json:"subject,omitempty"`
-	Content string   `json:"content"`
-	Type    string   `json:"type"`
-}
